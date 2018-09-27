4107128f25c9cf35acaa401f22662d1efc044c55
Nathan Salazar <nathan.salazar20@gmail.com>
2018-09-27 10:28:33 -0500

    set up pg and connect POST route to database

--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const pool = require('../modules/pool.js');
 
 router.use(bodyParser.json());
 
 router.post('/',(req,res)=>{
     console.log('req.body is',req.body);
-    res.send(200);
+    pool.query(`INSERT INTO "movies"
+        ("title", "genre_id", "release_date", "run_time")
+        VALUES ($1, $2, $3, $4);`,
+        [req.body.title, req.body.genre, req.body.date, req.body.runtime]
+    ).then((results)=>{
+        res.send(results.rows);
+    }).catch((error)=>{
+        console.log('Error in POST:',error);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
