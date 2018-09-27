1e8769531b913d28cc8e56a9def62dd6ee7e9f4a
Nathan Salazar <nathan.salazar20@gmail.com>
2018-09-27 08:57:09 -0500

    set up remaining folder structure and bring in angular-material

--- database.sql
+++ database.sql
@@ -1,3 +1,29 @@
+CREATE TABLE "movies" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(120) NOT NULL,
+	"genre_id" INT NOT NULL,
+	"release_date" DATE NOT NULL,
+	"run_time" INT NOT NULL,
+	"all_genres" VARCHAR(40)
+);
+
+INSERT INTO "movies" 
+("title", "genre_id", "release_date", "run_time", "all_genres")
+VALUES ('Citizen Kane', 9648, '1941-04-30', 119, '9648,18');
+
+CREATE TABLE "genre" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(20) NOT NULL,
+	"api_id" INT
+);
+
+INSERT INTO "genre"
+("name", "api_id")
+VALUES ('Action', 28);
+
+
+
+
 
 --this is my attempt at inserting JSON into a SQL table, and getting information back
 CREATE TABLE "testJSON" (
@@ -14,8 +40,8 @@ VALUES ('{"prop1": "soccer", "prop3": "hindenburg", "prop4": 10}', 'secondJSON')
 
 
 INSERT INTO "testJSON" ("jsonData", "string")
-VALUES ('{"prop2": "computer", "prop4": 15, "prop1": true}', 'secondJSON');
+VALUES ('{"prop2": "computer", "prop4": 15, "prop1": true}', 'thirdJSON');
 
 SELECT "jsonData"->>'prop1' AS prop1 FROM "testJSON";
 
-SELECT "jsonData"->>'prop4' AS prop1 FROM "testJSON";
+SELECT "jsonData"->>'prop4' AS prop4 FROM "testJSON";
