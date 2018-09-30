5e5b405dad494fddf5f843a5dd604476e7d6fb75
Nathan Salazar <nathan.salazar20@gmail.com>
2018-09-29 22:23:40 -0500

    update cards on genre page

--- server/routes/allInGenreRouter.js
+++ server/routes/allInGenreRouter.js
@@ -10,7 +10,7 @@ router.get('/:name', (req, res) => {
     console.log('In server GETting genres');
     console.log('req.params.name is',req.params.name);
     pool.query(`
-        SELECT "name", "genres"."tmdb_id", "title", "image" AS "poster_path" FROM "movies" 
+        SELECT "name", "genres"."tmdb_id", "title", "image", "run_time", "release_date" FROM "movies" 
         JOIN "movies_genres" ON "movies".tmdb_id="movies_genres".tmdb_id
         JOIN "genres" ON "genres".tmdb_id="movies_genres".genre
         WHERE "name"=$1;`,[req.params.name]).then((results) => {
