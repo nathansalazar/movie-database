e00d2233b1eb21de1babd787868377d22e68289c
Nathan Salazar <nathan.salazar20@gmail.com>
2018-10-02 12:52:57 -0500

    clean up .sql file

--- database.sql
+++ database.sql
@@ -2,60 +2,26 @@ CREATE TABLE "movies" (
 	"id" SERIAL PRIMARY KEY,
 	"title" VARCHAR(120) NOT NULL,
 	"genre_id" INT NOT NULL,
-	"release_date" DATE NOT NULL,
+	"release_date" DATE,
 	"run_time" INT NOT NULL,
-	"all_genres" VARCHAR(40)
+	"all_genres" VARCHAR(40),
+    "image" VARCHAR(255),
+    "tmdb_id" INT,
+    "rating" INT,
+    "user_rating" INT
 );
 
 INSERT INTO "movies" 
 ("title", "genre_id", "release_date", "run_time", "all_genres")
 VALUES ('Citizen Kane', 9648, '1941-04-30', 119, '9648,18');
 
-CREATE TABLE "genre" (
-	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR(20) NOT NULL,
-	"api_id" INT
-);
-
-INSERT INTO "genre"
-("name", "api_id")
-VALUES ('Action', 28);
-
---add a few more columns for the "movies" table
-ALTER TABLE "movies"
-ADD "image" VARCHAR(255);
-
-ALTER TABLE "movies"
-ADD "tmdb_id" INT;
-
---SQL query to get number of movies in each genre
-SELECT "name", COUNT("movies_genres".genre) FROM "movies" 
-JOIN "movies_genres" ON "movies".tmdb_id="movies_genres".tmdb_id
-JOIN "genres" ON "genres".tmdb_id="movies_genres".genre
-GROUP BY "movies_genres".genre, "genres"."name";
-
---SQL query to get all movies in a given genre
-SELECT "name", "genres"."tmdb_id", "title", "image" FROM "movies" 
-JOIN "movies_genres" ON "movies".tmdb_id="movies_genres".tmdb_id
-JOIN "genres" ON "genres".tmdb_id="movies_genres".genre
-WHERE "name"='Animation';
-
-
---create movies_genres junction table, using the api's id for movie and genre
-CREATE TABLE "movies_genres" (
-"tmdb_id" INT NOT NULL,
-"genre" INT NOT NULL
-);
-
-
-
---Recreate "genres" table to include the api's genre ids and the populate it
 CREATE TABLE "genres" (
 "id" SERIAL PRIMARY KEY,
 "name" VARCHAR(20) NOT NULL,
 "tmdb_id" INT NOT NULL
 );
 
+--insert all genres and their ids from TMDB (The Movie Database API)
 INSERT INTO "genres"
 ("tmdb_id", "name")
 VALUES
@@ -79,18 +45,20 @@ VALUES
     (10752,'War'),
 (37,'Western');
 
---add column to "movies" table for average rating
-ALTER TABLE "movies"
-ADD "rating" INT;
+--SQL query to get number of movies in each genre
+SELECT "name", COUNT("movies_genres".genre) FROM "movies" 
+JOIN "movies_genres" ON "movies".tmdb_id="movies_genres".tmdb_id
+JOIN "genres" ON "genres".tmdb_id="movies_genres".genre
+GROUP BY "movies_genres".genre, "genres"."name";
 
---remove condition that "release_date" is required
-ALTER TABLE "movies" ALTER COLUMN "release_date" DROP NOT NULL;
+--SQL query to get all movies in a given genre
+SELECT "name", "genres"."tmdb_id", "title", "image" FROM "movies" 
+JOIN "movies_genres" ON "movies".tmdb_id="movies_genres".tmdb_id
+JOIN "genres" ON "genres".tmdb_id="movies_genres".genre
+WHERE "name"='Animation';
 
-
---add user_rating column to "movies" table and insert data 
-ALTER TABLE "movies"
-ADD "user_rating" INT;
-
-UPDATE "movies"
-SET "user_rating"=87
-WHERE "tmdb_id"=105;
+--create movies_genres junction table, using the api's id for movie and genre
+CREATE TABLE "movies_genres" (
+"tmdb_id" INT NOT NULL,
+"genre" INT NOT NULL
+);
